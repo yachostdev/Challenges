cc9db37b4a35cd12fd4395456ec701d3064a275f
Yeison Castellanos <yachost@gmail.com>
2023-07-19 20:22:33 -0500

    Changes

--- challenge-one-news-homepage/src/components/Header.jsx
+++ challenge-one-news-homepage/src/components/Header.jsx
@@ -1,6 +1,12 @@
+import { Navbar } from './Navbar'
+import logo from '../assets/images/logo.svg'
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header className=''>
+      <img src={logo} alt="logo" />
+      <Navbar/>
+    </header>
   )
 }
 
---
+++ challenge-one-news-homepage/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+export const Navbar = () => {
+  return (
+    <ul className="bg-blue-400 hidden sm:flex ">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">New</a>
+      </li>
+      <li>
+        <a href="#">Popular</a>
+      </li>
+      <li>
+        <a href="#">Trending</a>
+      </li>
+      <li>
+        <a href="#">Categories</a>
+      </li>
+    </ul>
+  );
+};
